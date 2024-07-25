7ed06b936fec670450af5f47c08baea2a2488b0e
PerryMoris <morison.nasia@gmail.com>
2024-07-25 10:23:41 +0000

    model

--- hmsys/models.py
+++ hmsys/models.py
@@ -1,4 +1,6 @@
+from typing import Any
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Client (models.Model):
@@ -22,7 +24,8 @@ class Category (models.Model):
 
 class Rooms (models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=False)
-    number = models.CharField(max_length=255, null=False, blank=True)
+    number = models.CharField(max_length=255, null=True, blank=False)
+    lan_line = models.CharField(max_length=255, null=True, blank=True)
     amount = models.PositiveIntegerField(null=True, blank=False)
     occupied = models.BooleanField(default=False)
 
@@ -39,7 +42,33 @@ class Booked (models.Model):
     def __str__(self):
         return f"{self.client.get_full_name()} - {self.room}"
 
-class Payments (models.Model):
-    pass
+class Payments(models.Model):
+    mode = models.CharField(max_length=150, null=True, blank=True)
+    booked = models.ForeignKey(Booked, on_delete=models.CASCADE, null=True, blank=True)
+    amount_due = models.PositiveIntegerField(null=True, blank=True)
+    amount_paid = models.PositiveIntegerField(null=True, blank=True)
+    fully_paid = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if self.amount_due == self.amount_paid:
+            self.fully_paid = True
+        else:
+            self.fully_paid = False
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.booked} - {self.amount_paid}"
+
+
+class Service_Request(models.Model):
+    booked = models.ForeignKey(Booked, on_delete=models.CASCADE)
+    requested = models.TextField(null=True, blank=True)
+    delivered = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.booked.client} - {self.requested:[70]}"
     
+class Cleaning(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
